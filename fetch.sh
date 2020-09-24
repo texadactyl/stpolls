d4f608e85996d5125bcb6dd8af407767a9d0275c
Richard Elkins <richard.elkins@gmail.com>
2020-09-24 07:37:11 -0500

    Improved fetch.sh handling of data subdirectory

--- fetch.sh
+++ fetch.sh
@@ -12,13 +12,11 @@ LOCAL_CSV_FILE='data/input.csv'
 if [ -d data]; then
     rm -rf data
 fi
-
-if [ -r $LOCAL_CSV_FILE ]; then
-    rm $LOCAL_CSV_FILE
-fi
+mkdir data
 
 wget 'https://www.electoral-vote.com/evp2020/Pres/'$INTERNET_CSV_FILE
 if [ $? -ne 0 ]; then 
     oops "wget failed"
 fi
 mv $INTERNET_CSV_FILE $LOCAL_CSV_FILE
+
